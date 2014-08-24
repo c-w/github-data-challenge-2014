7acfeb21a5c75c4e63f1ab9a34902b37615bb1bf
c-w <clemens.wolff+git@gmail.com>
2014-08-24 02:32:47 +0100

    Filter out auto-generated avatars

--- hog/preprocess_avatars.sh
+++ hog/preprocess_avatars.sh
@@ -3,6 +3,7 @@
 #
 # Takes the previously downloaded avatars and applies pre-processing: size
 # reduction, color conversion, etc.
+# Also filters out any auto-generated avatars (two-color, game-of-life-style).
 #
 # Usage:
 #   preprocess_avatars.sh avatar...  # pre-processes the avatars
@@ -14,6 +15,7 @@
 IMAGE_SIZE='100x100'      # size to which to convert the avatars
 IMAGE_TYPE='png'          # the image type to which to conver the avatars
 IMAGE_COLOR='gray'        # the colorspace to which to transform the avatars
+AUTOGEN_DIR='data/autogen'  # directory to which to move auto-generated avatars
 
 
 ###############################################################################
@@ -27,6 +29,8 @@ log() {
 
     echo "[${now}] ${message}" 2>&1
 }
+
+sizeof() { du "$1" | cut -f1; }
 
 convert_image() {
     local image_orig="$1"
@@ -51,10 +55,18 @@ main() {
     set -o nounset
     set -o pipefail
 
+    # setup
+    mkdir -p "${AUTOGEN_DIR}"
+
     # convert images
     for image_orig in ${images}; do
-        local image_out="$(convert_image ${image_orig})"
-        log "converted ${image_orig} to ${image_out}"
+        if [ $(sizeof "${image_orig}") -le 12 ]; then
+            mv "${image_orig}" "${AUTOGEN_DIR}"
+            log "filtered auto-generated avatar ${image_orig} to ${AUTOGEN_DIR}"
+        else
+            local image_out="$(convert_image ${image_orig})"
+            log "converted ${image_orig} to ${image_out}"
+        fi
     done
 }
 
